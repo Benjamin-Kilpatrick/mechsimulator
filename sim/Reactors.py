1bb5b5f1de8f56255c303031fe1e40d7bddff671
Brown <Tristan_Brown1@baylor.edu>
2025-03-24 18:52:56 -0500

    branch for reactors file

---
+++ sim/Reactors.py
@@ -0,0 +1,64 @@
+
+
+# used ---- to split functions for my own view for now, so i know where functions are that were inside of other functions
+
+class Reactors():
+
+    @staticmethod
+    def st():
+        raise NotImplementedError
+
+    @staticmethod
+    def _wall_velocity(time):
+        raise NotImplementedError
+
+    # ---------------
+
+    @staticmethod
+    def rcm():
+        raise NotImplementedError
+
+    @staticmethod
+    def _piston_velocity(time):
+        raise NotImplementedError
+
+    # ---------------
+
+    @staticmethod
+    def pfr():
+        raise NotImplementedError
+
+    # ---------------
+
+    @staticmethod
+    def jsr():
+        raise NotImplementedError
+
+    # ---------------
+
+    @staticmethod
+    def const_t_p():
+        raise NotImplementedError
+
+    # ---------------
+
+    @staticmethod
+    def free_flame():
+        raise NotImplementedError
+
+    # ---------------
+
+    @staticmethod
+    def burner():
+        #TODO: BRO WHAT IS THIS?!?
+        pass
+
+    #---------------
+    @staticmethod
+    def _set_state():
+        raise NotImplementedError
+
+    @staticmethod
+    def _mix_str():
+        raise NotImplementedError
+
