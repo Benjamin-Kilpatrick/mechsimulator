8db0f8929a4dc3f8b34f9f8616e4459ca320d6d4
Benjamin Kilpatrick <befk500@gmail.com>
2025-03-30 09:27:40 -0500

    DataSource comments

--- data/experiments/common/data_source.py
+++ data/experiments/common/data_source.py
@@ -3,9 +3,13 @@ from enum import Enum
 
 class DataSource(Enum):
     SIMULATION = 0
-    """simulation source"""
+    """simulation source
+    Designated as plot or plots in the Job file.
+    """
     MEASURED = 1
-    """measured source"""
+    """measured source
+    Designated as exp or exps in the Job file.
+    """
 
     INVALID = -1
     """invalid source"""
