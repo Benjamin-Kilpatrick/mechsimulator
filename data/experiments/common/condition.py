6875ce63eb0c893e16487899d9606febff0f4ef4
unknown <mcar3550@students.kleinisd.net>
2025-04-10 14:31:57 -0500

    added has function to experiment set

--- data/experiments/common/condition.py
+++ data/experiments/common/condition.py
@@ -1,55 +1,67 @@
 from enum import Enum
 
+import pint
+
+from data.experiments.common.idt_method import IDTMethod
+from data.experiments.common.idt_target import IDTTarget
+
 
 class Condition(Enum):
     """
     Variable types used in Experiments
     """
-    TIME_STEP = 0
+    TIME_STEP = (0, pint.Quantity)
     """time step"""
-    END_TIME = 1
+    END_TIME = (1, pint.Quantity)
     """end time"""
-    WAVELENGTH = 2
+    WAVELENGTH = (2, pint.Quantity)
     """wavelength"""
-    ACTIVE_SPECIES = 3
-    """active species"""
-    ABS_COEFFICIENT = 4
+
+    # to be removed
+    # ACTIVE_SPECIES = 3
+    # """active species"""
+
+    ABS_COEFFICIENT = (4, pint.Quantity)
     """absorption coefficient"""
-    PATH_LENGTH = 5
+    PATH_LENGTH = (5, pint.Quantity)
     """path length"""
-    IGNITION_DELAY_TARGETS = 6
-    """ignition delay targets"""
-    IGNITION_DELAY_METHOD = 7
+
+    # to be removed
+    IGNITION_DELAY_TARGET = (6, IDTTarget)
+    """ignition delay objective"""
+
+    IGNITION_DELAY_METHOD = (7, IDTMethod)
     """ignitiion delay method"""
 
     # not actually a variable
     # TARGET_SPECIES = 8
     # """target species"""
-    TEMPERATURE = 9
+
+    TEMPERATURE = (9, pint.Quantity)
     """temperature"""
-    PRESSURE = 10
+    PRESSURE = (10, pint.Quantity)
     """pressure"""
-    PHI = 11
+    PHI = (11, pint.Quantity)
     """phi"""
-    DPDT = 12
+    DPDT = (12, pint.Quantity)
     """dPdT"""
-    LENGTH = 13
+    LENGTH = (13, pint.Quantity)
     """length"""
-    RES_TIME = 14
+    RES_TIME = (14, pint.Quantity)
     """res time"""
-    MDOT = 15
+    MDOT = (15, pint.Quantity)
     """mdot"""
-    AREA = 16
+    AREA = (16, pint.Quantity)
     """area"""
-    VOLUME = 17
+    VOLUME = (17, pint.Quantity)
     """volume"""
-    TIME = 18
+    TIME = (18, pint.Quantity)
     """time"""
-    V_OF_T = 19
+    V_OF_T = (19, pint.Quantity)
     """v of t"""
-    X_PROFILE = 20
+    X_PROFILE = (20, pint.Quantity)
     """x profile"""
-    TIME_PROFILE = 21
+    TIME_PROFILE = (21, pint.Quantity)
     """time profile"""
-    TIME_PROFILE_SETPOINTS = 22
+    TIME_PROFILE_SETPOINTS = (22, pint.Quantity)
     """time profile set points"""
