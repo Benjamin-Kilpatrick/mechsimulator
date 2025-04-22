518180f83a7e7be0e9f901c053b0c92bab3b9f8c
unknown <mcar3550@students.kleinisd.net>
2025-04-22 14:37:55 -0500

    added copy to experiment set and updated simulator

--- sim/Simulator.py
+++ sim/Simulator.py
@@ -16,22 +16,27 @@ class Simulator:
     sensitivity_simulator: SensitivitySimulator = SensitivitySimulator()
 
     @staticmethod
-    def run_experiment_set(experiment_set: ExperimentSet, mechanisms: List[Mechanism]):
+    def run_experiment_set(experiment_set: ExperimentSet, mechanisms: List[Mechanism]) -> List[ExperimentSet]:
+        simulated_experiment_sets: List[ExperimentSet] = []
         for mechanism in mechanisms:
-            SimulatorUtils.rename_all_species(experiment_set, mechanism)
-            if experiment_set.calculation_type == CalculationType.OUTCOME or experiment_set.calculation_type == CalculationType.PATHWAY:
-                Simulator.outcome_simulator.simulate_experiments(experiment_set, experiment_set.all_simulated_experiments[0], mechanism)
+            curr_experiment_set: ExperimentSet = experiment_set.copy()
+            SimulatorUtils.rename_all_species(curr_experiment_set, mechanism)
+            if curr_experiment_set.calculation_type == CalculationType.OUTCOME or curr_experiment_set.calculation_type == CalculationType.PATHWAY:
+                Simulator.outcome_simulator.simulate_experiments(curr_experiment_set, curr_experiment_set.all_simulated_experiments[0], mechanism)
 
-            elif experiment_set.calculation_type == CalculationType.SENSITIVITY:
-                Simulator.outcome_simulator.simulate_experiments(experiment_set, experiment_set.all_simulated_experiments[0], mechanism)
-                if experiment_set.reaction == Reaction.SHOCKTUBE:
+            elif curr_experiment_set.calculation_type == CalculationType.SENSITIVITY:
+                Simulator.outcome_simulator.simulate_experiments(curr_experiment_set, curr_experiment_set.all_simulated_experiments[0], mechanism)
+                if curr_experiment_set.reaction == Reaction.SHOCKTUBE:
                     # add epsilon to prevent divide by 0
-                    experiment_set.all_simulated_experiments[0] += numpy.finfo(float).eps
-                for reaction_ndx, experiments in enumerate(experiment_set.all_simulated_experiments[1:]):
+                    curr_experiment_set.all_simulated_experiments[0] += numpy.finfo(float).eps
+                for reaction_ndx, experiments in enumerate(curr_experiment_set.all_simulated_experiments[1:]):
                     mechanism.solution.set_multiplier(1.0)  # Reset all multipliers to original values
                     mechanism.solution.set_multiplier(1 + SensitivitySimulator.FACTOR, reaction_ndx)
-                    Simulator.sensitivity_simulator.simulate_experiments(experiment_set, experiments, mechanism)
+                    Simulator.sensitivity_simulator.simulate_experiments(curr_experiment_set, experiments, mechanism)
 
             else:
-                raise NotImplementedError(f"{experiment_set.calculation_type} does not have a simulator associated with "
+                raise NotImplementedError(f"{curr_experiment_set.calculation_type} does not have a simulator associated with "
                                           f"it yet.")
+            simulated_experiment_sets.append(curr_experiment_set)
+
+        return simulated_experiment_sets
